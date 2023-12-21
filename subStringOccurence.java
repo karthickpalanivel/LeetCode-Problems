3858f3c9273c12a3e58ca9cb0e4a5fe1b0c7586c
Karthick2003 <karthickpalanivel4@gmail.com>
2023-12-21 08:06:53 +0530

    modified substring occurence problem

--- subStringOccurence.java
+++ subStringOccurence.java
@@ -1,39 +1,15 @@
 import java.util.*;
 public class subStringOccurence {
     public static void main(String[] args) {
-        
-        
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        System.out.println(solution(word));
+        sc.close();
     }
 
     public static boolean solution(String str) {
-        int n = str.length();
-        str = str.toLowerCase();
-        
-        if (n == 0 || n == 1) {
-            return false;
-        }
-
-        HashSet<Character> set = new HashSet<Character>();
-        char[] ch = str.toCharArray();
-        
-
-        for (int i = 0; i < n; i++) {
-            set.add(ch[i]);
-        }
-
-        char[] ch1 = new char[set.size()];
-
-        for (char i : set) {
-            ch1[i] = i;
-        }
-
-        String subString = new String(ch1);
-
-        System.out.print(subString);
-        if (str.contains(subString + subString)) {
-            return true;
-        } else {
-            return false;
-        }
+        String dou = str + str;
+        String sub = dou.substring(1, dou.length() - 1);
+        return sub.contains(str);
     }
 }
