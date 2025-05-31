000d1b298c6c628a1f18ca73e98cab665fa6f73a
karthick <karthickpalanivel4@gmail.com>
2025-05-31 19:21:41 +0530

    soemthing

---
+++ LeetCode1004.java
@@ -0,0 +1,16 @@
+public class LeetCode1004 {
+    public static int solution(int[] ones, int k){
+        int result = 0;
+
+        
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1,1,1,0,0,0,1,1,1,1,0};
+        int[] arr2 = {0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
+
+        System.out.println(solution(arr1, 3));
+        System.out.println(solution(arr2, 2));
+    }
+}
